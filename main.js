0184c9394e8201a35d43d0085c948124977f35fc
Chris Clark <clark800@users.noreply.github.com>
2023-09-18 17:39:53 -0700

    Add build and install scripts

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var exports = {};
+
+if (typeof arguments !== 'undefined') {
+    for (var i = 0; i < arguments.length; i++) {
+        var input = readFile(arguments[i]);
+        var result = minify(input);
+        if (result.error) {
+            print();
+            throw new Error(result.error);
+        }
+        print(result.code);
+    }
+}
